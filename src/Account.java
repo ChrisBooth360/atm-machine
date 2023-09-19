5de6a28a0c23e107d9754888694bd422cf4ffd40
chrisbooth <chris.booth.ct@gmail.com>
2023-09-19 12:10:52 +0200

    Setting up account information

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Account {
 
     int accountNum;
@@ -7,9 +9,63 @@ public class Account {
 
     Account(int accountNum, int pinNum){
         this.accountNum = accountNum;
-        this.pinNum = accountNum;
+        this.pinNum = pinNum;
         this.savingsTotal = 0.0;
         this.chequeTotal = 0.0;
+    }
+
+    public int getAccountNum(){
+        return accountNum;
+    }
+
+    public int getPinNum(){
+        return pinNum;
+    }
+
+    public double getSavingsTotal(){
+        
+        return savingsTotal;
+    }
+
+    public double getChequeTotal(){
+        return chequeTotal;
+    }
+
+    public void setSavingsTotal(double newSavings){
+        
+        double newSavingsTotal = savingsTotal + newSavings;
+        
+        savingsTotal = newSavingsTotal;
+    }
+
+    public void setChequeTotal(double newCheque){
+
+        double newChequeTotal = chequeTotal + newCheque;
+        chequeTotal = newChequeTotal;
+    }
+
+    public String getSavingsString(){
+        return "Your total savings is " + getMoneyFormat(savingsTotal);
+    }
+
+    public String getChequeString(){
+        return "Your total chequing is " + getMoneyFormat(chequeTotal);
+    }
+
+    public String getTypeSavings(){
+        return "Savings";
+    }
+
+    public String getTypeChequing(){
+        return "Chequing";
+    }
+
+    public String getMoneyFormat(double moneyDouble){
+
+        DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.0");
+        String formattedNum = moneyFormat.format(moneyDouble);
+        return formattedNum;
+
     }
 
     
--- src/OptionMenu.java
+++ src/OptionMenu.java
@@ -46,9 +46,72 @@ public class OptionMenu {
 
   }
 
-  public void accountSelection(Account curreAccount){
+  public void accountSelection(Account currentAccount){
 
-    
+    Scanner input2 = new Scanner(System.in);
+    String userChoice = "0";
+
+    do{
+
+      System.out.println("Which account would you like to access? Input number as choice.");
+      System.out.println("1 - Savings Account");
+      System.out.println("2 - Chequing Account");
+      System.out.println("3 - exit");
+
+      userChoice = input2.nextLine();
+
+      switch(userChoice){
+        case "1":
+          actionSelection(currentAccount, currentAccount.getSavingsTotal(), currentAccount.getTypeSavings());
+          break;
+        case "2":
+          actionSelection(currentAccount, currentAccount.getChequeTotal(), currentAccount.getTypeChequing());
+          break;
+        case "3":
+          userChoice = "3";
+          break;
+        default:
+          System.out.println("You have made an invalid choice. Try again.");
+          break;
+      }
+
+    } while(userChoice != "3");
+
+  }
+
+  public void actionSelection(Account currentAccount, double accountTypeTotal, String accountType){
+
+    Scanner input2 = new Scanner(System.in);
+    String userChoice = "0";
+    double userAmount = 0.0;
+
+    do{
+
+      System.out.println(accountType + " Account.");
+      System.out.println("1 - View balance");
+      System.out.println("2 - Withdraw amount");
+      System.out.println("3 - Deposit amount");
+      System.out.println("4 - Go back");
+
+      userChoice = input2.nextLine();
+
+      if(userChoice == "1" && accountType == "Chequing"){
+        currentAccount.getChequeString();
+      } else if(userChoice == "1" && accountType == "Savings"){
+        currentAccount.getSavingsString();
+      } else if(userChoice == "2" && accountType == "Chequing"){
+          System.out.println("How much would you like to withdraw? (Type -1 to go back)");
+          userAmount = input2.nextDouble();
+          if(userAmount == -1){
+            break;
+          } else {
+            System.out.println("You've withdrawn " + currentAccount.getMoneyFormat(userAmount));
+            System.out.println("Y");
+          }
+      }
+
+    } while(userChoice != "3");
+
 
   }
 
